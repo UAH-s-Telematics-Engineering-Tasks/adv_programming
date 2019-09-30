cd1f22e757765ebc1a174455d5ec3c33a31f0dca
Pablo Collado <pablo@pop-os.localdomain>
2019-09-30 23:03:52 +0200

    Tidying up!

--- Windows_versions/P_2_a/Utils.h
+++
@@ -1,4 +0,0 @@
-int read_compare_int(char*, bool, int);
-int read_int(char*);
-float read_float(char*);
-int show_menu(const char**, int);
--- Windows_versions/P_2_b/Library_includes.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include <new>
-#include <limits>
-#include <vector>

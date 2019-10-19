cca34fab84bc17a8f82ff6f61dcf8ad9a58bec85
Pablo Collado <pcolladosoto@gmail.com>
2019-10-19 20:29:07 +0200

    Declared functions as inline. Finished P_5 :)

--- P_5/inc/P_5_Index_class.h
+++ P_5/inc/P_5_Index_class.h
@@ -13,12 +13,12 @@ namespace Index_info {
 
     public:
       Index_class(const std::string& = "", int = 0, int = 12, int = 0, int = 0, const std::string& = "AM");
-      inline void set_name(const std::string&);
-      inline std::string get_name(void) const;
-      inline void set_age(int&);
-      inline int get_age(void) const;
-      inline bool set_birth_time(int, int, int, std::string&);
-      inline Hour::Hour_class get_birth_time(void) const;
+      inline void set_name(const std::string& name) {this->index_name = name;}
+      inline std::string get_name(void) const {return this->index_name;}
+      inline void set_age(int& age) {this->index_age = age;}
+      inline int get_age(void) const {return this->index_age;}
+      inline bool set_birth_time(int h, int m, int s, std::string& f) {return this->index_birth_time.set_time(h, m, s, f);}
+      inline Hour::Hour_class get_birth_time(void) const {return this->index_birth_time;}
   };
   void view_index(const Index_class&);
 }
--- P_5/src/P_5_Index_class.cpp
+++ P_5/src/P_5_Index_class.cpp
@@ -1,34 +1,7 @@
 #include "../inc/P_5_Index_class.h"
 
 
-Index_info::Index_class::Index_class(const std::string& name, int age, int hours, int minutes, int seconds, const std::string& time_format) : index_name{name}, index_age{age}, index_birth_time{hours, minutes, seconds, time_format} {
-  std::cout << "Calling Index_class' regular constructor!\n";
-}
-
-inline void Index_info::Index_class::set_name(const std::string& name) {
-  this->index_name = name;
-}
-
-inline std::string Index_info::Index_class::get_name(void) const {
-  return this->index_name;
-}
-
-inline void Index_info::Index_class::set_age(int& age) {
-  this->index_age = age;
-}
-
-inline int Index_info::Index_class::get_age(void) const {
-  return this->index_age;
-}
-
-inline bool Index_info::Index_class::set_birth_time(int h, int m, int s, std::string& tf) {
-  return this->index_birth_time.set_time(h, m, s, tf);
-}
-
-inline Hour::Hour_class Index_info::Index_class::get_birth_time(void) const {
-  //This will invoke the copy constructor on main()!
-  return this->index_birth_time;
-}
+Index_info::Index_class::Index_class(const std::string& name, int age, int hours, int minutes, int seconds, const std::string& time_format) : index_name{name}, index_age{age}, index_birth_time{hours, minutes, seconds, time_format} {}
 
 void Index_info::view_index(const Index_class& index) {
   std::cout << "Name: " << index.get_name() << " Age: " << index.get_age() << " Birth time: ";

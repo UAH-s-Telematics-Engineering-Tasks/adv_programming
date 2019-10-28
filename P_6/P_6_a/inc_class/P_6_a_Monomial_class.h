eaa9a8431a098e9ec474365cec6cd012c77633c4
Pablo Collado <pcolladosoto@gmail.com>
2019-10-28 13:55:38 +0100

    Began...

---
+++ P_6/P_6_a/inc_class/P_6_a_Monomial_class.h
@@ -0,0 +1,24 @@
+#include ""
+
+#if !defined(__MONOMIAL_CLASS__)
+#define __MONOMIAL_CLASS__
+
+class Monomial_class {
+  private:
+    double coefficient;
+    int exponent;
+
+  public:
+    Monomial_class(double coeff = 0, int exp = 0) : coefficient{coeff}, exponent{exp} {}
+
+    double get_coeff(void) const {return this->coefficient;}
+    int get_exp(void) const {return this->exponent;}
+    void set_coeff(double coeff) {this->coefficient = coeff;}
+    void set_exp(int exp) {this->exponent = exp;}
+
+    Monomial_class operator-(void) const {return Monomial_class((this->coefficient), this->exponent);}
+};
+
+std::ostream& operator<<(std::ostream&, const Monomial_class&);
+
+#endif
---
+++ P_6/P_6_a/inc_class/P_6_a_Term_class.h
@@ -0,0 +1,10 @@
+#include ""
+
+#if !defined(__TERM_CLASS__)
+#define __TERM_CLASS__
+
+class Term_class {
+
+};
+
+#endif

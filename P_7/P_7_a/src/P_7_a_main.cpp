8a1360585c2cb98eae200979a148b50495c20c93
Pablo <pcolladosoto@gmail.com>
2019-12-01 22:07:20 +0100

    Added colors to the output :lipstick:

--- P_7/P_7_a/src/P_7_a_main.cpp
+++ P_7/P_7_a/src/P_7_a_main.cpp
@@ -46,6 +46,7 @@ int main(void) {
   Record_class* backup = NULL;
   int data_index = 0;
   std::string str_buff = "";
+  bool flag;
   /* This call is needed for each employee or customer. As it is going to be incredibly cumbersome to fill in the data during tests we are just going to "harcode" some values and pass them */
   /* Employee_class new_emp(utils::Cpp_utils::read_string(str_buff, "Name: "), utils::General_utils::read_int("Age: "), utils::General_utils::read_int("Hours: "), utils::General_utils::read_int("Minutes: "),
   utils::General_utils::read_int("Seconds: "),
@@ -57,9 +58,9 @@ int main(void) {
         Employee_class new_emp(names[data_index % DATA_SIZE], ages[data_index % DATA_SIZE], 12, 0, 0, "AM", positions[data_index % DATA_SIZE], wrk_y[data_index % DATA_SIZE]);
         data_index++;
         if (record.add_person(&new_emp))
-          std::cout << "Current reocords: " << record.get_next_free();
+          std::cout << "\033[38;2;0;255;255m" << "Current records: " << record.get_next_free() << " " << "\033[0m";
         else
-          std::cout << "I'm kind of full...";
+          std::cout << "I'm kind of full... ";
         std::getchar();
       }
         break;
@@ -68,27 +69,32 @@ int main(void) {
         Customer_class new_cust(names[data_index % DATA_SIZE], ages[data_index % DATA_SIZE], 12, 0, 0, "AM", DNIs[data_index % DATA_SIZE]);
         data_index++;
         if (record.add_person(&new_cust))
-          std::cout << "Current reocords: " << record.get_next_free();
+          std::cout << "\033[38;2;0;255;255m" << "Current records: " << record.get_next_free() << " " << "\033[0m";
         else
-          std::cout << "I'm kind of full...";
+          std::cout << "I'm kind of full... ";
         std::getchar();
       }
         break;
 
       case 3:
         if (!record.get_next_free()) {
-          std::cout << "We have an empty record...";
+          std::cout << "\033[38;2;255;0;0m" << "We have an empty record... " << "\033[0m";
           std::getchar();
           break;
         }
         utils::Cpp_utils::read_string(str_buff, "Name to look for: ");
-        for (int i = record.get_next_free() - 1; i >= 0; i--)
+        flag = true;
+        for (int i = record.get_next_free() - 1; i >= 0 && flag; i--)
           if (record[i]->get_name() == str_buff) {
             record[i]->show();
-            if (Record_class::is_employee(record[i]))
-              std::cout << "\tI'm an employee!";
-            else
-              std::cout << "\tI'm a customer!";
+            if (Record_class::is_employee(record[i])) {
+              std::cout << "\033[38;2;0;255;0m" << "\tI'm an employee!" << "\033[0m";
+              flag = false;
+            }
+            else {
+              std::cout << "\033[38;2;0;255;0m" << "\tI'm a customer!" << "\033[0m";
+              flag = false;
+            }   
           }
         std::getchar();
         break;
@@ -108,9 +114,11 @@ int main(void) {
         if (!backup) {
           if(!(backup = new (std::nothrow) Record_class(record)))
             std::cout << "Error while backing up...\n";
+          std::cout << "\033[38;2;0;255;0m" << "Backing up... " << "\033[0m";
         }
         else
-          std::cout << "We already have a backup!\n";
+          std::cout << "\033[38;2;255;0;0m" << "We already have a backup! " << "\033[0m";
+        std::getchar();
         break;
 
       case 7:
@@ -119,9 +127,11 @@ int main(void) {
           record = *backup;
           delete backup;
           backup = NULL;
+          std::cout << "\033[38;2;0;255;0m" << "Restored the backup! " << "\033[0m";
         }
         else
-          std::cout << "We don't have a backup yet...\n";
+          std::cout << "\033[38;2;255;0;0m" << "We don't have a backup yet... " << "\033[0m";
+        std::getchar();
         break;
 
       case 8:
